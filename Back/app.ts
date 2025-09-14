89f1ec24ca91a0648d3cf2c3f16f4020e7055583
Alec Shehorn <69906934+ashehorn@users.noreply.github.com>
2025-09-14 01:34:38 -0500

    Update app.ts

--- Back/app.ts
+++ Back/app.ts
@@ -17,7 +17,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 const corsOptions = {
-	origin: "http://localhost:5173",
+	origin: ["http://localhost:5173",
+			"https://project-7-34g6.onrender.com",
+			],
 	credentials: true,
 	methods: "GET,POST,PUT,DELETE",
 	allowedHeaders: ["Content-Type", "Authorization"],
